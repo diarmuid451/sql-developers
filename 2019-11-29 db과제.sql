0ec98d67f4b3bd349370d4afb0ec7385e67a3d52
diarmuid. J <diarmuid451@gmail.com>
2019-11-29 18:19:15 +0900

    2019-11-29
    db

--- 2019-11-29 db과제.sql
+++ 2019-11-29 db과제.sql
@@ -19,3 +19,29 @@ from countries JOIN regions ON(REGIONS.REGION_ID = countries.region_id)
                 JOIN locations ON(countries.COUNTRY_ID = locations.country_id)
 where regions.REGION_ID = 1;
 
+--join10
+select r.REGION_ID, r.REGION_NAME, c.COUNTRY_NAME, l.CITY, d.DEPARTMENT_NAME
+from countries c, regions r, locations l, departments d
+where r.REGION_ID = c.REGION_ID AND c.COUNTRY_ID = l.country_id 
+    AND l.LOCATION_ID = d.LOCATION_ID AND r.REGION_ID = 1;
+
+
+--join11
+select *
+from employees;
+
+select r.REGION_ID, r.REGION_NAME, c.COUNTRY_NAME, l.CITY, d.DEPARTMENT_NAME, 
+CONCAT(e.FIRST_NAME,e.LAST_NAME) name
+from countries c, regions r, locations l, departments d, EMPLOYEES e
+where r.REGION_ID = c.REGION_ID AND c.COUNTRY_ID = l.country_id 
+    AND l.LOCATION_ID = d.LOCATION_ID AND d.DEPARTMENT_ID = e.department_id ANd r.REGION_ID = 1
+ORDER BY l.location_id;
+
+--join12
+select e.EMPLOYEE_ID, CONCAT(e.FIRST_NAME,e.LAST_NAME) name, e.job_id, j.JOB_TITLE
+from employees e JOIN jobs j ON(e.job_id = j.job_id);
+
+--join13
+select *
+from EMPLOYEES;
+
